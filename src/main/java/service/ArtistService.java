16892a28ee2f32277992fcf09443e3368ad6f37c
KohaDeadinside <149077645+KohaDeadinside@users.noreply.github.com>
2023-11-12 15:30:16 +0700

    Update ArtistService.java
    fix syntax

--- src/main/java/service/ArtistService.java
+++ src/main/java/service/ArtistService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ArtistService {
     List<Artist> getAllArtist();
-    Artist addArtist()
+    Artist addArtist();
     Artist getAllArtistById(long id);
     void deleteArtistById(long id);
     void saveArtist(Artist artist);
