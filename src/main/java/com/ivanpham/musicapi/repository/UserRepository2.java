14f8120a8593fcdd7de0410f35c152506d59aabc
Khoa <52100969@student.tdtu.edu.vn>
2023-11-27 22:01:31 +0700

    add method findByUsername

--- src/main/java/com/ivanpham/musicapi/repository/UserRepository2.java
+++ src/main/java/com/ivanpham/musicapi/repository/UserRepository2.java
@@ -1,9 +1,10 @@
 package com.ivanpham.musicapi.repository;
 
-import com.ivanpham.musicapi.model.Album;
 import com.ivanpham.musicapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository2  extends JpaRepository<User, String> {
     User findByEmail(String mail);
+    User findByUsername(String userName);
 }
