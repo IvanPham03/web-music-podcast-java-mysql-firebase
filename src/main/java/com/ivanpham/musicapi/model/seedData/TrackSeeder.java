b27d74eeaae05c65626c6f3f5d82320b23489501
Khoa <52100969@student.tdtu.edu.vn>
2023-12-05 14:20:31 +0700

    add data seeder (track, user, userTrack)

--- src/main/java/com/ivanpham/musicapi/model/seedData/TrackSeeder.java
+++ src/main/java/com/ivanpham/musicapi/model/seedData/TrackSeeder.java
@@ -32,15 +32,76 @@ public class TrackSeeder {
 
     private List<Track> createTracks() {
         List<Track> tracks = new ArrayList<>();
-        User user = userRepository2.findByEmail("admin1@gmail.com");
+        User user1 = userRepository2.findByEmail("admin1@gmail.com");
+//        User user2 = userRepository2.findByEmail("");
 
-        if(user != null){
+        if(user1 != null){
             // Tạo các đối tượng Track và thêm vào danh sách tracks
-            Track track1 = new Track("Anh đã yên bình, tôi biết thương mình", "AnhDaYenBinhToiBietThuongMinh-PhamQuynhAnh-9010380.mp3","ballad", user);
-            Track track2 = new Track("Anh sẽ đưa em về", "AnhSeDuaEmVe-NQP-6309479.mp3", "ballad", user);
+            Track track1 = new Track("Anh đã yên bình, tôi biết thương mình", "AnhDaYenBinhToiBietThuongMinh-PhamQuynhAnh-9010380.mp3","ballad", user1);
+            Track track2 = new Track("Anh sẽ đưa em về", "AnhSeDuaEmVe-NQP-6309479.mp3", "ballad", user1);
+
+            Track track3 = new Track("Ân tình sang trang", "AnTinhSangTrang-ChauKhaiPhongLeCuong-7976352.mp3", "ballad", user1);
+            Track track4 = new Track("Chiêm bao cuối", "ChiemBaoCuoi-Keyo-5992899.mp3", "ballad", user1);
+            Track track5 = new Track("Chúng ta bấy lâu nay là", "ChungTaBayLauNayLa-PhamQuynhAnh-9046058.mp3", "ballad", user1);
+            Track track6 = new Track("Cũng xin là", "CungXinLa-TrangHanNamKun-5962285.mp3", "ballad", user1);
+            Track track7 = new Track("Đớn đau anh vẫn yêu", "DonDauAnhVanYeu-ChauKhaiPhong-4166657.mp3", "ballad", user1);
+            Track track8 = new Track("Dreaming boy", "DreamingBoy-HallasanVietNamSeachains-7561734.mp3", "pop", user1);
+            Track track9 = new Track("Em sẽ là của anh", "EmSeLaCuaAnh-EmDCindyLeDavis-6103424.mp3", "ballad", user1);
+            Track track10 = new Track("Falling in love", "FallingInLove-QuocAnhVietNam-7266712.mp3", "pop", user1);
+            Track track11 = new Track("Kết thúc không vui", "KetThucKhongVui-ChauKhaiPhong-2746549.mp3", "ballad", user1);
+            Track track12 = new Track("Kẻ viết ngôn tình", "KeVietNgonTinh-ChauKhaiPhongACV-9558544.mp3", "ballad", user1);
+            Track track13 = new Track("Không trọn vẹn nữa", "KhongTronVenNua-ChauKhaiPhongACV-7197914.mp3", "ballad", user1);
+            Track track14 = new Track("Liêm sĩ gì tầm này", "LiemSiGiTamNay-LinhQueenNonHanTa-6223913.mp3", "ballad", user1);
+            Track track15 = new Track("Liêm sĩ gì tầm này", "LiemSiGiTamNay-MisabaeTungViu-6504570.mp3", "ballad", user1);
+            Track track16 = new Track("Love mood", "LoveMood-KhoaLangLD-6241843.mp3", "ballad", user1);
+            Track track17 = new Track("Mãi là giấc mơ", "MaiLaGiacMo-DinhUyenLanPham-4680054.mp3", "ballad", user1);
+            Track track18 = new Track("Này em gì ơi", "NayEmGiOi-NguyenKhoaLangLD-6136321.mp3", "ballad", user1);
+            Track track19 = new Track("Ngắm hoa lệ rơi", "NgamHoaLeRoi-ChauKhaiPhong-4850041.mp3", "ballad", user1);
+            Track track20 = new Track("Người ta thành đôi hết rồi đấy", "NguoiTaThanhDoiHetRoiDay-TheSheepTRI-6059865.mp3", "ballad", user1);
+            Track track21 = new Track("Nỗi đau mình anh", "NoiDauMinhAnh-ChauKhaiPhongTrinhDinhQuang-4361102.mp3", "ballad", user1);
+            Track track22 = new Track("Not a fairy tale", "NotAFairyTale-OrangeDTAP-9217147.mp3", "pop", user1);
+            Track track23 = new Track("Quên một người từng yêu", "QuenMotNguoiTungYeu-ChauKhaiPhong-6352300.mp3", "ballad", user1);
+            Track track24 = new Track("Say anh", "SayAnh-MyMyDlowVietNam-7565091.mp3", "ballad", user1);
+            Track track25 = new Track("Thế là thôi", "TheLaThoi-HoangPhuongTraMy-4178173.mp3", "ballad", user1);
+            Track track26 = new Track("Thương em", "ThuongEm-ChauKhaiPhongACV-7803607.mp3", "ballad", user1);
+            Track track27 = new Track("Thương thầm", "ThuongTham-RickyStarKhoa-6464375.mp3", "ballad", user1);
+            Track track28 = new Track("Tìm Acoustic cover", "TimAcousticCover-Rhy-4184464.mp3", "ballad", user1);
+            Track track29 = new Track("Tỏ tình", "ToTinh-JBHaCuong-6183895.mp3", "ballad", user1);
+            Track track30 = new Track("Yêu là thế", "YeuLaThe-UMIERightTeeBPBOUNCE-6229650.mp3", "ballad", user1);
+
 
             tracks.add(track1);
             tracks.add(track2);
+
+            tracks.add(track3);
+            tracks.add(track4);
+            tracks.add(track5);
+            tracks.add(track6);
+            tracks.add(track7);
+            tracks.add(track8);
+            tracks.add(track9);
+            tracks.add(track10);
+            tracks.add(track11);
+            tracks.add(track12);
+            tracks.add(track13);
+            tracks.add(track14);
+            tracks.add(track15);
+            tracks.add(track16);
+            tracks.add(track17);
+            tracks.add(track18);
+            tracks.add(track19);
+            tracks.add(track20);
+            tracks.add(track21);
+            tracks.add(track22);
+            tracks.add(track23);
+            tracks.add(track24);
+            tracks.add(track25);
+            tracks.add(track26);
+            tracks.add(track27);
+            tracks.add(track28);
+            tracks.add(track29);
+            tracks.add(track30);
+
         }
 
 
--- src/main/java/com/ivanpham/musicapi/model/seedData/UserSeeder.java
+++ src/main/java/com/ivanpham/musicapi/model/seedData/UserSeeder.java
@@ -52,10 +52,21 @@ public class UserSeeder {
         // Tạo các đối tượng user
         User admin1 = new User("admin1@gmail.com", "admin1", passwordEncoder.encode("admin1"), Arrays.asList(role_admin));
         User admin2 = new User("admin2@gmail.com", "admin2", passwordEncoder.encode("admin2"), Arrays.asList(role_admin));
+
         User artist1 = new User("artist1@gmail.com", "artist1", passwordEncoder.encode("artist1"), Arrays.asList(role_artist));
         User artist2 = new User("artist2@gmail.com", "artist2", passwordEncoder.encode("artist2"), Arrays.asList(role_artist));
+       User artist3 = new User("artist3@gmail.com", "artist3", passwordEncoder.encode("artist3"), Arrays.asList(role_artist));
+       User artist4 = new User("artist4@gmail.com", "artist4", passwordEncoder.encode("artist4"), Arrays.asList(role_artist));
+       User artist5 = new User("artist5@gmail.com", "artist5", passwordEncoder.encode("artist5"), Arrays.asList(role_artist));
+       User artist6 = new User("artist6@gmail.com", "artist6", passwordEncoder.encode("artist6"), Arrays.asList(role_artist));
+
+
         User user1 = new User("user1@gmail.com", "user1", passwordEncoder.encode("user1"), Arrays.asList(role_user));
-        User user2 = new User("user2@gmail.com", "user2", passwordEncoder.encode("user2"),Arrays.asList(role_user));
+        User user2 = new User("user2@gmail.com", "user2", passwordEncoder.encode("user2"), Arrays.asList(role_user));
+       User user3 = new User("user3@gmail.com", "user3", passwordEncoder.encode("user3"), Arrays.asList(role_user));
+       User user4 = new User("user4@gmail.com", "user4", passwordEncoder.encode("user4"), Arrays.asList(role_user));
+       User user5 = new User("user5@gmail.com", "user5", passwordEncoder.encode("user5"), Arrays.asList(role_user));
+       User user6 = new User("user6@gmail.com", "user6", passwordEncoder.encode("user6"), Arrays.asList(role_user));
 
         users.add(user1);
         users.add(admin1);
@@ -64,6 +75,16 @@ public class UserSeeder {
         users.add(admin2);
         users.add(artist1);
         users.add(artist2);
+
+       users.add(artist3);
+       users.add(artist4);
+       users.add(artist5);
+       users.add(artist6);
+
+       users.add(user3);
+       users.add(user4);
+       users.add(user5);
+       users.add(user6);
 
         return users;
     }
