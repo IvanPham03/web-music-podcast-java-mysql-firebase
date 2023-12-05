70e07a88fb55df768eac1fef6c1b05496f0cb732
Khoa <52100969@student.tdtu.edu.vn>
2023-12-05 18:54:28 +0700

    add data seeding cho PlaylistSeeder.java, UserPlaylistSeeder.java, UserTrackSeeder.java

--- src/main/java/com/ivanpham/musicapi/model/seedData/PlaylistSeeder.java
+++ src/main/java/com/ivanpham/musicapi/model/seedData/PlaylistSeeder.java
@@ -22,25 +22,39 @@ public class PlaylistSeeder {
 
     public void seedPlaylist() {
         if (playlistRepository.count() == 0) {
-            List<Playlist> playlists = createUsers();
+            List<Playlist> playlists = createPlaylist();
 
             // Lưu danh sách tracks vào cơ sở dữ liệu
             playlistRepository.saveAll(playlists);
         }
     }
 
-    private List<Playlist> createUsers() {
+    private List<Playlist> createPlaylist() {
         List<Playlist> playlists = new ArrayList<>();
 
-        User user = userRepository2.findByEmail("admin1@gmail.com");
-        if(user != null){
+        User user1 = userRepository2.findByEmail("admin1@gmail.com");
+        User user2 = userRepository2.findByUsername("user2");
+        User user3 = userRepository2.findByUsername("user3");
+        User user4 = userRepository2.findByUsername("user4");
+
+
+        if(user1 != null && user2 != null && user3 != null && user4 != null){
             // Tạo các đối tượng Playlist
-            Playlist playlist1 = new Playlist("playlist1", user , "public");
-            Playlist playlist2 = new Playlist("playlist2", user , "public");
-            Playlist playlist3 = new Playlist("playlist3", user, "privacy");
+            Playlist playlist1 = new Playlist("playlist1", user1 , "public");
+            Playlist playlist2 = new Playlist("playlist2", user1 , "public");
+            Playlist playlist3 = new Playlist("playlist3", user1, "privacy");
+
+            Playlist playlist4 = new Playlist("playlist4", user2 , "private");
+            Playlist playlist5 = new Playlist("playlist5", user3 , "private");
+            Playlist playlist6 = new Playlist("playlist6", user4, "privacy");
+
             playlists.add(playlist1);
             playlists.add(playlist2);
             playlists.add(playlist3);
+
+            playlists.add(playlist4);
+            playlists.add(playlist5);
+            playlists.add(playlist6);
 
         }
 
